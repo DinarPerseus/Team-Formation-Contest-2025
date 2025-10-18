abe733be178fa462ccc705014e88a501c387fbe3
Dinar Jahan <110754416+DinarPerseus@users.noreply.github.com>
2025-10-18 18:13:04 +0600

    Add files via upload

---
+++ D একটু বিয়োগ, একটুখানি শান্তি.cpp
@@ -0,0 +1,40 @@
+// Dinar Perseüs 2.0
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+void fileio(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+} 
+ 
+void solve(int t){
+    ll n,k,s=0;
+    cin>>n>>k;
+    if(k==1){
+        cout<<n<<endl;
+        return ;
+    }
+    while(n){
+        s+=n%k;
+        n/=k;
+    }
+    cout<<s<<endl;
+    
+}
+
+
+
+int main()
+{
+    fileio();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // clock_t z = clock();
+    int t = 1;
+    cin>>t;
+    for (int i = 1; i <= t; i++)solve(i);
+    // cerr<<"Run Time : "<<((double)(clock()-z)/CLOCKS_PER_SEC);
+    return 0;
+}
